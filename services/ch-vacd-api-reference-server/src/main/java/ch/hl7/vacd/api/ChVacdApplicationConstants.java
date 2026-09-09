f28aed2b4fbe8e0814da4f29ef2d7636f752aa02
Roeland Luykx <roeland.luykx@raly.ch>
2026-09-09 11:52:33 +0200

    extract constants to own class

---
+++ services/ch-vacd-api-reference-server/src/main/java/ch/hl7/vacd/api/ChVacdApplicationConstants.java
@@ -0,0 +1,7 @@
+package ch.hl7.vacd.api;
+
+public class ChVacdApplicationConstants {
+
+	public static final String AHV_OID = "urn:oid:2.16.756.5.32";
+	public static final String GLN_OID = "urn:oid:2.51.1.3";
+}
